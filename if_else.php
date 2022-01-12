430887dc695134686e2b1fb1e2688d6a7c91b1e5
JunjungHasudungan <junjunghasudungan43@gmail.com>
2022-01-12 20:57:12 +0700

    second commit

---
+++ if_else.php
@@ -0,0 +1,13 @@
+<?php
+
+$numb = 10;
+if($numb <= 11)
+{
+    echo $numb . "lebih kecil";
+}
+elseif($numb >= 11)
+{
+    echo $numb. "lebih besar";
+}else{
+    echo $numb ."sama dengan nilai 11";
+}
---
+++ switch_case.php
@@ -0,0 +1,29 @@
+<?php
+
+$hari = "jumat";
+
+switch($hari){
+    case 1:
+        echo "hari minggu";
+    break;
+    case 2:
+        echo "hari senin";
+    break;
+    case 3:
+        echo "hari selasa";
+    break;
+    case 4:
+        echo "hari rabu";
+    break;
+    case 5:
+        echo "hari kamis";
+    break;
+    case 6:
+        echo "hari jumat";
+    break;
+    case 7:
+        echo "hari sabtu";
+    break;
+    default:
+        echo "angka tidak tersedia";
+}
