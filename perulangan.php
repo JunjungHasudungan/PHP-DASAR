f26dcea97ba4340d425f9d7e65814f105de3a689
JunjungHasudungan <junjunghasudungan43@gmail.com>
2022-01-17 11:33:04 +0700

    pertemuan ke 3

--- perulangan.php
+++ perulangan.php
@@ -1,3 +1,17 @@
 
 <?php
 
+// mengunakan for
+
+function loopFor()
+{
+    $salam = "halo";
+    $jumlah = 5;
+
+    for($i = 1; $i <= $jumlah; $i++)
+    {
+        echo $i." ".$salam ."<br>";
+    }
+}
+
+loopFor();
